438b4728928e3be974dc3463b2c6b2060c0391bc
Zaid Bouslikhin <zaid@mx3d.com>
2024-12-16 13:11:25 +0100

    feat: use lazyvim's extras lang

--- config/home-manager/editors/neovim/lazyvim/lua/config/lazy.lua
+++ config/home-manager/editors/neovim/lazyvim/lua/config/lazy.lua
@@ -12,7 +12,8 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    -- { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
 
     -- import/override with your plugins
     { import = "plugins" },
