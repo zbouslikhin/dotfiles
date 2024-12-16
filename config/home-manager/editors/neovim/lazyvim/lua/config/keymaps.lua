7b63c6474efe6ceb24b496906677a4703f34c8bf
Zaid Bouslikhin <zaid@mx3d.com>
2024-12-16 13:12:05 +0100

    fix: clean keymaps

--- config/home-manager/editors/neovim/lazyvim/lua/config/keymaps.lua
+++ config/home-manager/editors/neovim/lazyvim/lua/config/keymaps.lua
@@ -7,14 +7,14 @@ vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(event)
     local opts = { buffer = event.buf }
 
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, opts)
-
-    vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", opts)
-    vim.keymap.set("n", "gs", "<cmd>Telescope grep_string<cr>", opts)
-    vim.keymap.set("n", "gD", "<cmd>Telescope diagnostics<cr>", opts)
-
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    -- vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, opts)
+    --
+    -- vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", opts)
+    -- vim.keymap.set("n", "gs", "<cmd>Telescope grep_string<cr>", opts)
+    -- vim.keymap.set("n", "gD", "<cmd>Telescope diagnostics<cr>", opts)
+    --
+    -- vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    -- vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
   end,
 })
