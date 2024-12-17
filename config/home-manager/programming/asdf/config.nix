9c9715c10d0de9146b4d7e5b57cad782ff3df2c7
Zaid Bouslikhin <z.bouslikhin@gmail.com>
2024-12-18 00:23:52 +0100

    install nh

--- config/home-manager/programming/asdf/config.nix
+++ config/home-manager/programming/asdf/config.nix
@@ -3,12 +3,20 @@
 {
     home.packages = [
         pkgs.asdf-vm
-        pkgs.awscli
+        # pkgs.awscli
+        # pkgs.nh
+        # pkgs.pkg-config
     ];
+    programs.nh = {
+    enable = true;
+    # clean.enable = true;
+    # clean.extraArgs = "--keep-since 4d --keep 3";
+    # flake = "/home/user/my-nixos-config";
+  };
 
-    programs = {
-        zsh = {
-            initExtra = ". $HOME/.nix-profile/share/asdf-vm/asdf.sh";
-        };
-    };  
+#     programs = {
+#         zsh = {
+#             initExtra = ". $HOME/.nix-profile/share/asdf-vm/asdf.sh";
+#         };
+#     };  
 }
